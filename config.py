1b994a263000147d50db5b2d5ac04fcfd3433bfd
Harsha Somisetty <harshasomisetty7@gmail.com>
2021-08-30 12:14:45 -0400

    managing api keys

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import urllib.parse
+from constants import username, password
 
 class Config(object):
     DEBUG = False
     TESTING = False
 
 class ProductionConfig(Config):
-    MONGO_URI = "mongodb+srv://***REMOVED***:"+urllib.parse.quote("***REMOVED***")+"@cluster0.4pec2.mongodb.net/production?retryWrites=true&w=majority"
+    MONGO_URI = "mongodb+srv://"+username+":"+urllib.parse.quote(password)+"@cluster0.4pec2.mongodb.net/production?retryWrites=true&w=majority"
     DEBUG = True # Turns on debugging features in Flask
 
 class DevelopmentConfig(Config):
     FLASK_ENV="development"
     DEBUG = True # Turns on debugging features in Flask
-    MONGO_URI = "mongodb+srv://***REMOVED***:"+urllib.parse.quote("***REMOVED***")+"@cluster0.4pec2.mongodb.net/development?retryWrites=true&w=majority"
+    MONGO_URI = "mongodb+srv://"+username+":"+urllib.parse.quote(password)+"@cluster0.4pec2.mongodb.net/development?retryWrites=true&w=majority"
